a45c13c2189e77c600d9056a9bdaae172111bcae
Cristopherson Torres <cristopherson.torres@gmail.com>
2015-02-04 00:40:20 -0600

    Modify Environment and TileAgent classes

--- src/agents/Environment.java
+++ src/agents/Environment.java
@@ -1,5 +1,11 @@
 package agents;
 
+import info.gridworld.actor.Bug;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import utilities.BugAgent;
 import utilities.TileGame;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
@@ -11,9 +17,11 @@ public class Environment extends Agent{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private TileGame tileGame;;
+	private TileGame tileGame;
+	private HashMap <String, BugAgent>map;
 	
 	public Environment() {
+		map = new HashMap<String, BugAgent>();
 		tileGame = new TileGame();
 		tileGame.show();
 	}
@@ -28,12 +36,25 @@ public class Environment extends Agent{
 			public void action() {
 				ACLMessage msgRx = receive();
 				if (msgRx != null) {
-					if (msgRx.getPerformative() == ACLMessage.PROPOSE) {
+					if (msgRx.getPerformative() == ACLMessage.INFORM) {
 						System.out.println(msgRx);
 						ACLMessage msgTx = msgRx.createReply();
-						msgTx.setContent("Hello! " + msgRx.getSender().getName());
+						msgTx.setPerformative(ACLMessage.CONFIRM);
+						msgTx.setContent("Welcome " + msgRx.getSender().getLocalName());
 						send(msgTx);
+					} else if (msgRx.getPerformative() == ACLMessage.REQUEST) {
+						System.out.println(msgRx);
+						BugAgent bugAgent = new BugAgent(msgRx.getSender().getLocalName().toString(), (Environment)myAgent);
+												
+						map.put(bugAgent.getAgentName(), bugAgent);
+						tileGame.add(tileGame.getRandomEmptyLocation(), bugAgent);
+						
+						ACLMessage msgTx = msgRx.createReply();
+						msgTx.setPerformative(ACLMessage.AGREE);
+						msgTx.setContent("Agent " + bugAgent.getAgentName() + " has a place in this world");
+						send(msgTx);						
 					}
+					
 				} else {
 					block();
 				}
--- src/agents/TileAgent.java
+++ src/agents/TileAgent.java
@@ -1,5 +1,12 @@
 package agents;
 
+import behavior.AgreeTileRequest;
+import behavior.ConfirmExistence;
+import behavior.GameOver;
+import behavior.InformExistence;
+import behavior.InternalStateBehavior;
+import behavior.RequestTile;
+import behavior.TileAgentStates;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
@@ -10,21 +17,51 @@ public class TileAgent extends Agent {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private InternalStateBehavior internalState;
+	private InformExistence informExistence;
+	private ConfirmExistence confirmExistence;
+	private RequestTile requestTile;
+	private AgreeTileRequest agreeTileRequest;
+	private GameOver gameOver;
+	
+	public TileAgent() {
+		internalState = new InternalStateBehavior();
+		informExistence = new InformExistence();
+		confirmExistence = new ConfirmExistence();
+		requestTile = new RequestTile();
+		agreeTileRequest = new AgreeTileRequest();
+		gameOver = new GameOver();
+	}
 
 	public void setup() {
 		System.out.println("Hello. My name is " + getLocalName() + "\n");
-		addBehaviour(new OneShotBehaviour() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			public void action() {
-				ACLMessage msgTx = new ACLMessage(ACLMessage.PROPOSE);
-				msgTx.setContent("Hello!");
-				msgTx.addReceiver(new AID("Env", AID.ISLOCALNAME));
-				send(msgTx);
-			}
-		});
+		
+		internalState.registerFirstState(informExistence, TileAgentStates.INFORM_EXISTENCE);
+		internalState.registerLastState(gameOver, TileAgentStates.GAME_OVER);
+		internalState.registerState(confirmExistence, TileAgentStates.CONFIRM_EXISTENCE);
+		internalState.registerState(requestTile, TileAgentStates.REQUEST_TILE);
+		internalState.registerState(agreeTileRequest, TileAgentStates.AGREE_TILE_REQUEST);
+		
+		internalState.registerTransition(
+				TileAgentStates.INFORM_EXISTENCE,
+				TileAgentStates.CONFIRM_EXISTENCE,
+				TileAgentStates.INFORM_EXISTENCE_EVENT);
+		
+		internalState.registerTransition(
+				TileAgentStates.CONFIRM_EXISTENCE,
+				TileAgentStates.REQUEST_TILE,
+				TileAgentStates.CONFIRM_EXISTENCE_EVENT);
+		
+		internalState.registerTransition(
+				TileAgentStates.REQUEST_TILE,
+				TileAgentStates.AGREE_TILE_REQUEST,
+				TileAgentStates.REQUEST_TILE_EVENT);
+		
+		internalState.registerTransition(
+				TileAgentStates.AGREE_TILE_REQUEST,
+				TileAgentStates.GAME_OVER,
+				TileAgentStates.GAME_OVER_EVENT);
+		
+		addBehaviour(internalState);
 	}
 }
