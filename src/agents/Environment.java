012e1b941fb7eaa54e2ae2aaa595ecd124726e85
Cristopherson Torres <cristopherson.torres@gmail.com>
2015-02-05 00:37:49 -0600

    Agent simulation

--- src/agents/Environment.java
+++ src/agents/Environment.java
@@ -2,6 +2,9 @@ package agents;
 
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 
 import java.util.HashMap;
@@ -24,7 +27,7 @@ public class Environment extends Agent {
 	private TileGame tileGame;
 	private HashMap<String, Location> map;
 
-	public Environment() {		
+	public Environment() {
 		setMap(new HashMap<String, Location>());
 		tileGame = new TileGame(this);
 		tileGame.show();
@@ -52,11 +55,12 @@ public class Environment extends Agent {
 						BugAgent bugAgent = new BugAgent(msgRx.getSender()
 								.getLocalName().toString(),
 								(Environment) myAgent);
-						Location randomLocation =tileGame.getRandomEmptyLocation(); 
+						Location randomLocation = tileGame
+								.getRandomEmptyLocation();
 						tileGame.add(randomLocation, bugAgent);
-						
+
 						getMap().put(bugAgent.getAgentName(), randomLocation);
-												
+
 						ACLMessage msgTx = msgRx.createReply();
 						msgTx.setPerformative(ACLMessage.AGREE);
 						msgTx.setContent("Agent " + bugAgent.getAgentName()
@@ -69,8 +73,7 @@ public class Environment extends Agent {
 						if (content.equals("random")) {
 							Location location = map.get(agentName);
 							if (location != null) {
-								int action = ((int) (new Random().nextDouble()
-										* (AgentAction.MOVE_SOUTH + 1)));
+								int action = ((int) (new Random().nextDouble() * (AgentAction.MOVE_SOUTH + 1)));
 								executeAction(action, location);
 							}
 						}
@@ -93,43 +96,78 @@ public class Environment extends Agent {
 
 	public void executeAction(int action, Location location) {
 		Location adjacentLocation = null;
-		BugAgent bugAgent = (BugAgent)tileGame.getGrid().get(location);
-		
-		System.out.println("Current location " +  location);
+		BugAgent bugAgent = (BugAgent) tileGame.getGrid().get(location);
+		boolean failure = true;
+
+		System.out.println("Current location " + location);
 		if (action == AgentAction.MOVE_LEFT) {
-			adjacentLocation = location.getAdjacentLocation(Location.LEFT);			
+			action = Location.LEFT;
 		} else if (action == AgentAction.MOVE_RIGHT) {
-			adjacentLocation = location.getAdjacentLocation(Location.RIGHT);
+			action = Location.RIGHT;
 		} else if (action == AgentAction.MOVE_NORTH) {
-			adjacentLocation = location.getAdjacentLocation(Location.NORTH);
+			action = Location.NORTH;
 		} else if (action == AgentAction.MOVE_SOUTH) {
-			adjacentLocation = location.getAdjacentLocation(Location.SOUTH);
+			action = Location.SOUTH;
 		}
-		
-		if(adjacentLocation != null && isValid(adjacentLocation)) {
-			tileGame.remove(location);			
-			tileGame.add(adjacentLocation, bugAgent);
-			map.put(bugAgent.getAgentName(), adjacentLocation);
-			
-			ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
-			msg.addReceiver(new AID(bugAgent.getAgentName(), AID.ISLOCALNAME));
-			msg.setContent("moveto:" + action);
-			send(msg);			
-		} else {
+
+		adjacentLocation = location.getAdjacentLocation(action);
+		if (adjacentLocation != null && isValid(action, adjacentLocation)) {
+			Actor actor = tileGame.getGrid().get(adjacentLocation);
+
+			if (actor == null) {
+				tileGame.remove(location);
+				tileGame.add(adjacentLocation, bugAgent);
+				map.put(bugAgent.getAgentName(), adjacentLocation);
+				failure = false;
+			} else if (actor.getClass() == Flower.class) {
+				Location nextLocation = adjacentLocation
+						.getAdjacentLocation(action);
+				if (tileGame.getGrid().isValid(nextLocation)) {
+					Actor nextActor = tileGame.getGrid().get(nextLocation);
+					if (nextActor == null) {
+						tileGame.remove(location);
+						tileGame.remove(adjacentLocation);
+						tileGame.add(adjacentLocation, bugAgent);
+						tileGame.add(nextLocation, actor);
+						map.put(bugAgent.getAgentName(), adjacentLocation);
+						failure = false;
+					} else if (nextActor.getClass() == Rock.class) {
+						tileGame.remove(location);
+						tileGame.remove(adjacentLocation);
+						tileGame.remove(nextLocation);
+						tileGame.add(adjacentLocation, bugAgent);
+						map.put(bugAgent.getAgentName(), adjacentLocation);
+						failure = false;
+					}
+				}
+			}
+		}
+
+		if (failure) {
 			ACLMessage msg = new ACLMessage(ACLMessage.FAILURE);
 			msg.addReceiver(new AID(bugAgent.getAgentName(), AID.ISLOCALNAME));
 			msg.setContent("moveto:" + action);
-			send(msg);			
+			send(msg);
+		} else {
+			ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
+			msg.addReceiver(new AID(bugAgent.getAgentName(), AID.ISLOCALNAME));
+			msg.setContent("moveto:" + action);
+			send(msg);
 		}
 	}
-	
-	private boolean isValid(Location adjacentLocation) {
+
+	private boolean isValid(int action, Location adjacentLocation) {
 		boolean valid = false;
-		
+
 		valid = tileGame.getGrid().isValid(adjacentLocation);
-		if (tileGame.getGrid().get(adjacentLocation) != null) 
-			valid = false;
-				
+		if (valid) {
+			Actor actor = tileGame.getGrid().get(adjacentLocation);
+			if (actor != null
+					&& (actor.getClass() == Rock.class || actor.getClass() == Critter.class)) {
+				valid = false;
+			}
+		}
+
 		return valid;
 	}
 }
